520a228138c8ed2c0c73f6fabadd8f85638e2a50
edwingsantos <edwing.santos@ucas-edu.net>
2026-03-27 09:51:10 -0600

    yes

---
+++ add _student.py
@@ -0,0 +1,21 @@
+#Es add student
+
+#make a funtion to sfae the names
+
+    #open the file path and wrtie with a new line as file
+
+    #make info be a tuple of info, (name, id #, grades etc)
+
+    #
+
+
+#make name be an input asking for the name
+
+#make ID be an input asking the user for their ID
+
+#
+
+
+
+
+#REMEBER THAT THE NAME OF WHERE THIS IS IS UPDATE PERSONAL LIBRARY
---
+++ main.py
@@ -0,0 +1,19 @@
+#ES main
+
+#print the welcome entry 
+
+# make a funtion for main
+    #make a while loop 
+        #input where the user whants to go
+
+        # if choice is 1 call the add_student option
+
+        #if choice is 2 call the add grade file 
+
+        #if choice is 3 call the view students recod file
+
+        #if choice is 4 call the view all sumery 
+
+        #if choice is 5 call the class summary 
+
+        #if choice is 6 break 
